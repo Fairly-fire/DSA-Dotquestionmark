74c9984dc91cee44378a6b5f813c234cc9e45ca4
Fairly-fire <67552032+Fairly-fire@users.noreply.github.com>
2021-08-12 23:38:46 +0530

    Update Code1_Greater number among 2 nos.cpp

--- Code1_Greater number among 2 nos.cpp
+++ Code1_Greater number among 2 nos.cpp
@@ -1,1 +1,25 @@
+#include<iostream>
 
+using namespace std;
+
+int main()
+{
+	int a, b;
+	
+	cout << "Please enter the Two Different Number  = ";
+	cin >> a >> b;
+	
+	if(a > b)
+	{
+    	cout << a << " is Greater Than " << b;  
+	}         
+	else if(b > a)
+	{
+		cout << b << " is Greater Than " << a;  
+  	}
+  	else
+  	{
+  		cout << "Both are Equal";
+	}
+ 	return 0;
+}
