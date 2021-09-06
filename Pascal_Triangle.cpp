6eb18bbbd271eb9a1b940580b0a2939029345559
Fairly-fire <67552032+Fairly-fire@users.noreply.github.com>
2021-09-06 20:43:31 +0530

    Update and rename Pascal Triangle to Pascal_Triangle.cpp

--- Pascal_Triangle.cpp
+++ Pascal_Triangle.cpp
@@ -22,20 +22,3 @@ int main()
     }
 }
 
-//RECURSION
-#include<iostream>
-using namespace std;
-
-void fun(int n){
-    if(n>0){
-        cout<<n<<" ";
-        fun(n-1);
-        cout<<n<<" ";
-    }
-}
-int main()
-    int n;
-{
-    cin>>n;
-    fun(n);
-}
