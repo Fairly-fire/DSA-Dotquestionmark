09ece56ce5becb48a798d2b59e263b0b2f8a7fe8
Fairly-fire <67552032+Fairly-fire@users.noreply.github.com>
2021-09-16 21:31:52 +0530

    Create New_year_chaos.cpp

---
+++ New_year_chaos.cpp
@@ -0,0 +1,120 @@
+
+//https://www.hackerrank.com/contests/dq-dsa/challenges/new-year-chaos/submissions/code/1336597530
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+vector<string> split(const string &);
+
+/*
+ * Complete the 'minimumBribes' function below.
+ *
+ * The function accepts INTEGER_ARRAY q as parameter.
+ */
+
+void minimumBribes(vector<int> q) {
+    int ans=0;
+    int flag=0;
+    for(int i=q.size()-1;i>=0;i--){
+        int key = q[i];
+        int j=i+1;
+        int max=0;
+        while((j<q.size()) && (key>q[j])){
+            if(max<2){
+                q[j-1]= q[j];
+                j++;
+                max++;
+                ans++;
+            }
+            else{
+                flag++;
+                break;
+            }
+        }
+        if(!flag)
+            q[j-1]= key;
+        else{
+            break;
+        }
+    }
+   if(!flag){
+       cout << ans <<"\n";
+   }
+   else{
+       cout << "Too chaotic" <<"\n";
+    }
+}
+
+int main()
+{
+    string t_temp;
+    getline(cin, t_temp);
+
+    int t = stoi(ltrim(rtrim(t_temp)));
+
+    for (int t_itr = 0; t_itr < t; t_itr++) {
+        string n_temp;
+        getline(cin, n_temp);
+
+        int n = stoi(ltrim(rtrim(n_temp)));
+
+        string q_temp_temp;
+        getline(cin, q_temp_temp);
+
+        vector<string> q_temp = split(rtrim(q_temp_temp));
+
+        vector<int> q(n);
+
+        for (int i = 0; i < n; i++) {
+            int q_item = stoi(q_temp[i]);
+
+            q[i] = q_item;
+        }
+
+        minimumBribes(q);
+    }
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
+
+vector<string> split(const string &str) {
+    vector<string> tokens;
+
+    string::size_type start = 0;
+    string::size_type end = 0;
+
+    while ((end = str.find(" ", start)) != string::npos) {
+        tokens.push_back(str.substr(start, end - start));
+
+        start = end + 1;
+    }
+
+    tokens.push_back(str.substr(start));
+
+    return tokens;
+}
